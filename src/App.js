4bbd6f78c686e0b4a2c229a7ca50c27335d7f813
AbishekGhalley <abishekghalley_ec20a2_18@dtu.ac.in>
2024-12-18 13:57:03 +0600

    New Text added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1 className="text-7xl text-center text-blue-400" >Hello world</h1>
       <Layout/>
-     
+      <h2>Hi Selise</h2>
     </div>
   );
 }
